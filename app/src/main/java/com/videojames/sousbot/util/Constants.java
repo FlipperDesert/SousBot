fb2e6db613cbdfc3c626be37b7644d973a7b1b5e
FlipperDesert <49339735+FlipperDesert@users.noreply.github.com>
2021-06-25 12:16:24 +0100

    Removed API keys
    Removed API keys to make main branch public

--- app/src/main/java/com/videojames/sousbot/util/Constants.java
+++ app/src/main/java/com/videojames/sousbot/util/Constants.java
@@ -9,16 +9,16 @@ Spoonacular keys are kept here as variables for convenience in case of a later n
 public class Constants {
 
 
-    public static String Spoon_API_key = "4fe803bdea774ceda17e70268b4906a5";
+    public static String Spoon_API_key = "INSERT KEY HERE";
 
 
-    public static String ibmUsername = "SousBot";
-    public static String ibmApiKey = "aJDK4hpDL5c11emh5D_40Ji4pg-zFVC2dxb6CPemp05H";
-    public static String ibmId = "e9605a3f-7840-42c8-a2dc-39a0bc433b0f";
-    public static String ibmUrl = "https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/fbf76d0c-6203-4df0-bb53-9b4ff4080294";
-    public static String ibmVersionDate = "2020-09-24";
-    public static String ibmTtsApi = "k_9-NULb0kF3RL8nyo0pAvq4hwDzzPD09nD8M4jmNbUo";
-    public static String ibmTtsUrl = "https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/d7cde3c5-347a-4d06-9325-58df24c2eeea";
-    public static String ibmSttapi = "wrC9bd4oUjEOYGyLmBuvC_QsXJ89toqzejDfkfACEMWg";
-    public static String ibmStturl = "https://api.eu-gb.speech-to-text.watson.cloud.ibm.com/instances/36a571ae-52d3-40be-92cc-92c522b3ffbe";
+    public static String ibmUsername = "INSERT KEY HERE";
+    public static String ibmApiKey = "INSERT KEY HERE";
+    public static String ibmId = "INSERT KEY HERE";
+    public static String ibmUrl = "INSERT KEY HERE";
+    public static String ibmVersionDate = "INSERT KEY HERE";
+    public static String ibmTtsApi = "INSERT KEY HERE";
+    public static String ibmTtsUrl = "INSERT KEY HERE";
+    public static String ibmSttapi = "INSERT KEY HERE";
+    public static String ibmStturl = "INSERT KEY HERE";
 }
